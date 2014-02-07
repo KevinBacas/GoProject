8c054130d3fb4171354f786aef9fddae6a059064
Coupdbluff <sang1@hotmail.fr>
2014-02-07 20:46:40 +0100

    presque fini pour le jouer_coup

--- src/go/partie.c
+++ src/go/partie.c
@@ -112,7 +112,67 @@ void jouer_coup(SPartie partie, SPosition position)
 	SPlateau* plateau = partie->p_courant;
 	SChaines* chaines = partie->chaines;
 
+	int compteurBizarre=0;
 
+	SList* list = listAdd(listInit(),(void*)&position);
+	SList* l1 = listInit();
+	SList* l2 = listInit();
+	SList* l3 = listInit();
+	SList* l4 = listInit();
+
+	SPion* pion = creerPion(&position, couleur_joueur);
+	SPosition* v_droit = voisinDroit(plateau,&position);
+	SPosition* v_gauche = voisinGauche(plateau,position);
+	SPosition* v_bas = voisinBas(plateau,position);
+	SPosition* v_haut = voisinHaut(plateau,position);
+
+	SChaine* chaine_droit = positionDansChaines(chaines,v_droit);
+	SChaine* chaine_gauche = positionDansChaines(chaines,v_gauche);
+	SChaine* chaine_bas = positionDansChaines(chaines,v_bas);
+	SChaine* chaine_haut = positionDansChaines(chaines,v_haut);
+
+	plateau_set(plateau, &position,couleur_joueur);
+
+	if(couleur_joueur==plateau_get(plateau,v_haut))
+	{
+		l1=listConcatUnique(chaine_haut,list);
+		compteurBizarre += 1;
+	}
+
+	if(couleur_joueur==plateau_get(plateau,v_gauche))
+	{
+		if(compteurBizarre==1) l2=listConcatUnique(chaine_gauche,l1);
+		else
+		{
+			l2 = listConcatUnique(chaine_gauche,list);
+		}
+		compteurBizarre += 2;
+	}
+	if(couleur_joueur==plateau_get(plateau,v_bas))
+	{
+		if(compteurBizarre>=2) l3 = listConcatUnique(chaine_bas,l2);
+		else
+		{
+			if(compteurBizarre==1) l3 = listConcatUnique(chaine_bas,l1);
+			else l3 = listConcatUnique(chaine_bas,list);
+		}
+		compteurBizarre+=4;
+	}
+	if(couleur_joueur==plateau_get(plateau,v_droit))
+	{
+		if(compteurBizarre>=4) l4 = listConcatUnique(chaine_droit,l3);
+		else
+		{
+			if(compteurBizarre>=2) l4 = listConcatUnique(chaine_droit,l2);
+			else
+			{
+				if(compteurBizarre==1) l4=listConcatUnique(chaine_droit,l1);
+				else l4=listConcatUnique(chaine_droit,list);
+			}
+		}
+	}
+	//là il faut virer les autres chaines de la doublons de la liste de chaines
+	//tet que jme suis fait chier pour RIIIIEN !a voir
 }
 
 int coup_valide(SPartie* partie, SPosition* position)
@@ -138,34 +198,46 @@ int coup_valide(SPartie* partie, SPosition* position)
 	if(couleur_joueur != plateau_get(plateau,v_droit) && determineLiberte(plateau,chaine_droit)==NULL)
 	{
 		plateau_realiser_capture(plateau,chaines,chaine_droit);
+		detruirePion(pion);
 		return 1;
 	}
 	if(couleur_joueur != plateau_get(plateau,v_gauche) && determineLiberte(plateau,chaine_gauche)==NULL)
 	{
 		plateau_realiser_capture(plateau,chaines,chaine_gauche);
+		detruirePion(pion);
 		return 1;
 	}
 	if(couleur_joueur != plateau_get(plateau,v_bas)&& determineLiberte(plateau,chaine_bas)==NULL)
 	{
-			plateau_realiser_capture(plateau,chaines,chaine_bas);
+		plateau_realiser_capture(plateau,chaines,chaine_bas);
+		detruirePion(pion);
 		return 1;
 	}
 	if(couleur_joueur != plateau_get(plateau,v_haut) && determineLiberte(plateau,chaine_haut)==NULL)
 	{
 		plateau_realiser_capture(plateau,chaines,chaine_haut);
+		detruirePion(pion);
 		return 1;
 	}
 
 	if(listEmpty(libertesAdjacente(plateau,pion))==1)
 	{
 		if(couleur_joueur==plateau_get(plateau,v_haut) && determineLiberte(plateau,chaine_haut)==NULL)
+			detruirePion(pion);
 			return 0;
 		if(couleur_joueur==plateau_get(plateau,v_droit) && determineLiberte(plateau,chaine_droit)==NULL)
+			detruirePion(pion);
 			return 0;
 		if(couleur_joueur==plateau_get(plateau,v_gauche) && determineLiberte(plateau,chaine_gauche)==NULL)
+			detruirePion(pion);
 			return 0;
 		if(couleur_joueur==plateau_get(plateau,v_bas) && determineLiberte(plateau,chaine_bas)==NULL)
+			detruirePion(pion);
 			return 0;
 	}
-	else return 1;
+	else
+	{
+		detruirePion(pion);
+		return 1;
+	}
 }
